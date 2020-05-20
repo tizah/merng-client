8134f445c86b67839044131b45e0166787535876
tizah <davidzagi93@gmail.com>
2020-05-20 21:06:33 +0100

    edited app base url and added proxy

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from "@apollo/react-hooks";
 import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:5001",
+  uri: "https://arcane-fjord-03670.herokuapp.com/",
 });
 
 const authLink = setContext(() => {
